8d51914d2f1ab2b737fbcb71734208149872e2e0
Armeen <166984952+armeennadeem197@users.noreply.github.com>
2024-06-02 15:24:00 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer"
+
+const answers : {
+    Sentence : string
+ } = await inquirer.prompt(
+    {
+        name: "Sentence",
+        type : "input",
+        message : "Enter your sentence to count the word:"
+    }
+ )
+
+ const words = answers.Sentence.trim().split(" ")
+
+
+ console.log(words);
+ //print the words counts of the  sentence of the console
+ console.log(`your sentence word count is ${words.length}`);
+ 
